c75c1724877280925c02c93a120055c99f061e4c
tiamatt <samira.yusifova@gmail.com>
2017-10-23 19:53:14 -0400

    Create  Nav section

--- src/app/main/nav/nav.component.ts
+++ src/app/main/nav/nav.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
+  navArr : string[] = ['About', 'Skills', 'Projects', 'Experience', 'Activities', 'Contact'];
+  activeSection : string;
+  
   constructor() { }
 
   ngOnInit() {
+    this.initialValues();
+  }
+
+  initialValues(){
+    this.activeSection =  this.navArr[0];
+  }
+
+  onSectionClick(_selection: string){
+    this.activeSection = _selection;
   }
 
 }
