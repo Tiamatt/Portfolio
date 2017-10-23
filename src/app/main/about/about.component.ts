e6442b15cb8141c6c4f781dbe6c1394e7925d408
tiamatt <samira.yusifova@gmail.com>
2017-10-23 16:36:54 -0400

    Create About section

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -3,13 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css', '../../shared/styles/general.css']
 })
 export class AboutComponent implements OnInit {
-
   constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() { }
 }
